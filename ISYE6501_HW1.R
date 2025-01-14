4a4946b95bf05dcdb7d20023e90b253dd2e829bc
dylanmr1 <dylan.marcus.rivera@gmail.com>
2025-01-14 11:01:10 -0600

    Add files via upload

--- ISYE6501_HW1.R
+++ ISYE6501_HW1.R
@@ -2,18 +2,19 @@
 install.packages("kernlab")
 library(kernlab)
 
+path <- "~/Downloads/hw1/data 2.2/"
 # load dataset
-ccData <- read.csv("credit_card_data-headers.txt", sep = "")
+ccData <- read.csv(file.path(path,"credit_card_data-headers.txt"), sep="")
 
 # call ksvm. Vanilladot is a simple linear kernel.
-model <- ksvm(as.matrix(ccData[,1:10]), as.factor(ccData[,11]), type="C-svc", kernel="vanilladot", C=100, scaled=TRUE)
+model <- ksvm(as.matrix(ccData[,1:10]), as.factor(ccData[,11]), type="C-svc", kernel="vanilladot", C=100000, scaled=TRUE)
 
 # calculate a1…am
 a <- colSums(model@xmatrix[[1]] * model@coef[[1]])
 a
 
 # calculate a0
-a0 <- –model@b
+a0 <- -(model@b)
 a0
 
 # see what the model predicts
@@ -21,5 +22,5 @@ pred <- predict(model,ccData[,1:10])
 pred
 
 # see what fraction of the model’s predictions match the actual classification
-sum(pred == ccData[,11]) / nrow(ccData)
+fraction <- sum(pred == ccData[,11]) / nrow(ccData)
 
