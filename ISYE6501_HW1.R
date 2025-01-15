936da06743cd97909df6a398f551367098a9ecf6
dylanmr1 <dylan.marcus.rivera@gmail.com>
2025-01-15 06:33:13 -0600

    Add files via upload

--- ISYE6501_HW1.R
+++ ISYE6501_HW1.R
@@ -1,26 +1,45 @@
-# install and load package
+# install and load packages
 install.packages("kernlab")
 library(kernlab)
+library(ggplot2)
 
-path <- "~/Downloads/hw1/data 2.2/"
 # load dataset
-ccData <- read.csv(file.path(path,"credit_card_data-headers.txt"), sep="")
+setwd('/Users/Dylan Rivera/Desktop/OMSA/Spring 2025/ISYE 6501/HW1/')
+ccData <- read.csv("credit_card_data-headers.txt", sep = "")
 
-# call ksvm. Vanilladot is a simple linear kernel.
-model <- ksvm(as.matrix(ccData[,1:10]), as.factor(ccData[,11]), type="C-svc", kernel="vanilladot", C=100000, scaled=TRUE)
+# initialize empty data frame
+table = data.frame()
 
-# calculate a1…am
-a <- colSums(model@xmatrix[[1]] * model@coef[[1]])
-a
+# set initial value of C
+C <- 0.00001
 
-# calculate a0
-a0 <- -(model@b)
-a0
+# call ksvm, loop through different values of C
+while (C <= 100000) {
+  
+  # call ksvm function
+  model <- ksvm(as.matrix(ccData[,1:10]), as.factor(ccData[,11]), type="C-svc", kernel="vanilladot", C = C, scaled=TRUE)
+  
+  # calculate a1…am
+  a <- colSums(model@xmatrix[[1]] * model@coef[[1]])
+  a
+  
+  # calculate a0
+  a0 <- -(model@b)
+  a0
+  
+  # see what the model predicts
+  pred <- predict(model,ccData[,1:10])
+  pred
+  
+  # see what fraction of the model’s predictions match the actual classification
+  accuracy <- sum(pred == ccData[,11]) / nrow(ccData)
+  
+  # add value of C and model accuracy into table
+  row <- 
+  table <-
+  
+  # iterate to next value of C
+  C = C * 10
+}
 
-# see what the model predicts
-pred <- predict(model,ccData[,1:10])
-pred
-
-# see what fraction of the model’s predictions match the actual classification
-fraction <- sum(pred == ccData[,11]) / nrow(ccData)
-
+# plot graph of accuracy vs C value
